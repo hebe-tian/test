d181fc2b47c026a009189135301c67b39fcc52a4
hebe-tian <linleil@126.com>
2022-06-05 14:20:41 +0800

    class test login logout

--- test_page.py
+++ test_page.py
@@ -10,6 +10,7 @@ import json
 with open("data.json", 'r', encoding='utf-8') as f:
     data = json.load(f)
     url = data['url']
+
     xpath = data['page']
     movie_path = xpath['movie_path']
     book_path = xpath['book_path']
@@ -20,7 +21,23 @@ with open("data.json", 'r', encoding='utf-8') as f:
     about_me_path = xpath['about_me_path']
     about_me_text_path = xpath['about_me_text_path']
 
+    username_path = xpath['username_path']
+    password_path = xpath['password_path']
+    login_submit_path = xpath['login_submit_path']
+    alert_path = xpath['alert_path']
+    logout_path = xpath['logout_path']
+    settings_path = xpath['settings_path']
+    settings_text_path = xpath['settings_text_path']
+    f.close()
 
+with open("userdata.json", 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    name = data['username']
+    password = data['password']
+    f.close()
+
+
+@pytest.mark.skip()
 class TestPages:
 
     def setup(self):
@@ -48,6 +65,77 @@ class TestPages:
         self.driver.find_element_by_xpath(about_me_path).click()
         text = self.driver.find_element_by_xpath(about_me_text_path).text
         assert 'DiuDiu' == text
+
+    def teardown(self):
+        self.driver.quit()
+
+
+@pytest.mark.skip()
+class TestLogin:
+
+    def setup(self):
+        """open chrome and get url"""
+        options = webdriver.ChromeOptions()
+        options.add_argument('--incognito')
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get(url+'/login')
+
+    def test_login_without_all(self):
+        self.driver.find_element_by_xpath(login_submit_path).click()
+        text = self.driver.find_element_by_xpath(login_text_path).text
+        assert 'Login' in text
+
+    def test_login_wrong_msg(self):
+        self.driver.find_element_by_xpath(username_path).send_keys(name)
+        self.driver.find_element_by_xpath(password_path).send_keys('password')
+        self.driver.find_element_by_xpath(login_submit_path).click()
+        text = self.driver.find_element_by_xpath(alert_path).text
+        assert 'Invalid' in text
+
+    def test_login_right_msg(self):
+        self.driver.find_element_by_xpath(username_path).send_keys(name)
+        self.driver.find_element_by_xpath(password_path).send_keys(password)
+        self.driver.find_element_by_xpath(login_submit_path).click()
+        text = self.driver.find_element_by_xpath(alert_path).text
+        assert 'success' in text
+
+    def teardown(self):
+        self.driver.quit()
+
+
+class TestUser:
+
+    def setup(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--incognito')
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get(url+'/login')
+
+    def login(self):
+        self.driver.find_element_by_xpath(username_path).send_keys(name)
+        self.driver.find_element_by_xpath(password_path).send_keys(password)
+        self.driver.find_element_by_xpath(login_submit_path).click()
+
+    def test_not_login(self):
+        text = self.driver.find_element_by_xpath(logout_path).text
+        assert text != 'Logout'
+
+    def test_login(self):
+        self.login()
+        text = self.driver.find_element_by_xpath(logout_path).text
+        assert text == 'Logout'
+
+    def test_login_settings(self):
+        self.login()
+        self.driver.find_element_by_xpath(settings_path).click()
+        text = self.driver.find_element_by_xpath(settings_text_path).text
+        assert text == 'Settings'
+
+    def test_logout(self):
+        self.login()
+        self.driver.find_element_by_xpath(logout_path).click()
+        text = self.driver.find_element_by_xpath(alert_path).text
+        assert text == 'Goodbye.'
 
     def teardown(self):
         self.driver.quit()
